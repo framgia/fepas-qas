20d49cd5b14d2cffc3cd9e2fc276030a0a9461cd
Le Anh <le.ngoc.anh@framgia.com>
2016-04-30 21:20:06 +0700

    Create question with WYSIWYG editor

--- src/constants/question.js
+++ src/constants/question.js
@@ -1,4 +1,9 @@
 export default {
   // Actions
   QUESTION_DETAIL_GET: 'GET@@/question/detail',
+  // State
+  QUESTION_DATA_SUBMITTING: 'QUESTION_DATA_SUBMITTING',
+  QUESTION_DATA_UPDATED: 'QUESTION_DATA_UPDATED',
+  // Status
+  QUESTION_RESPONSE_FAILURE: '@@/question:CODE2XX',
 };
