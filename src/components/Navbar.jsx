b0f5692af92425fc78b2fe6e5928ba5edd1397a2
Tran Van Luan <tran.van.luan@framgia.com>
2016-05-15 18:51:17 +0700

    Task/63019 - Add Material-UI to Project and create layout + navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,57 +3,78 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { logoutUser } from '../actions/auth';
 import C from '../constants';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import Menu from 'material-ui/Menu';
+import FontIcon from 'material-ui/FontIcon';
+import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { cyan500, grey50 } from 'material-ui/styles/colors';
+import Avatar from 'material-ui/Avatar';
+
+const style = {
+  backgroundColor: cyan500,
+};
+
+injectTapEventPlugin();
 
 class Navbar extends Component {
+  getChildContext() {
+    return { muiTheme: getMuiTheme(baseTheme) };
+  }
   getRightNavbar(props) {
+    let avatar;
+    let menu;
     switch (props.auth.status) {
-      case C.AUTH_LOGGED_IN: return (
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <img className="profile-image img-circle img-responsive"
-              src={props.auth.profileImageUrl} alt={props.auth.username}
-            />
-          </li>
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <span className="caret"></span>
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <Link to="/profile">
-                  <i className="fa fa-user fa-fw"></i> Profile
-                </Link>
-              </li>
-              <li>
-                <Link to="/logout">
-                  <i className="fa fa-sign-out fa-fw"></i> Logout
-                </Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      );
-      default: return (
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <Link to="/auth">
-              <span className="glyphicon glyphicon-log-in"></span> Login
-            </Link>
-          </li>
-        </ul>
-      );
+      case C.AUTH_LOGGED_IN:
+        avatar = (
+          <Avatar
+            src={props.auth.profileImageUrl}
+            className="profile-image"
+          />
+        );
+        menu = (
+          <Menu>
+            <MenuItem><Link to="/profile">Profile</Link></MenuItem>
+            <MenuItem><Link to="/logout">Logout</Link></MenuItem>
+          </Menu>
+        );
+        break;
+      default:
+        menu = (
+          <MenuItem><Link to="/auth">Login</Link></MenuItem>
+        );
+        break;
     }
+    return (
+      <ToolbarGroup lastChild>
+        {avatar}
+        <IconMenu
+          iconButtonElement={
+            <IconButton touch>
+              <NavigationExpandMoreIcon color={grey50} />
+            </IconButton>
+          }
+        >
+          {menu}
+        </IconMenu>
+      </ToolbarGroup>
+    );
   }
   render() {
     return (
-      <nav className="navbar navbar-inverse">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link to="/" className="navbar-brand">Home</Link>
-          </div>
-          {this.getRightNavbar(this.props)}
-        </div>
-      </nav>
+      <Toolbar style={style}>
+        <ToolbarGroup firstChild>
+          <IconButton tooltip="Home">
+            <Link to="/"><FontIcon className="muidocs-icon-action-home" color={grey50} /></Link>
+          </IconButton>
+        </ToolbarGroup>
+        {this.getRightNavbar(this.props)}
+      </Toolbar>
     );
   }
 }
@@ -64,6 +85,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = {
   logoutUser,
+};
+
+Navbar.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
